9d8fff322907a5953c616f1ee5fe7719b813cd77
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2024-05-10 14:10:31 -0300

    ordering

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,49 @@
+<template>
+  <main>
+    <div class="login">
+      <div class="titulo text-default">
+        Login
+      </div>
+      <div class="mb-3">
+        <label for="formGroupExampleInput" class="text-default">Email</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Email" v-model="email">
+      </div>
+      <div class="mb-3">
+        <label for="formGroupExampleInput2" class="text-default">Senha</label>
+        <input type="password" class="form-control" id="formGroupExampleInput2" placeholder="Senha" v-model="password">
+      </div>
+      <div class="mb-3">
+        <button type="button" class="btn btn-outline-warning btn-sm" @click="login">Login</button>
+      </div>
+      <div class="mb-3">
+        <a href="/esqueceu-senha">Esqueceu a senha?</a>
+        <span> | </span>
+        <a href="/criar-conta">Criar conta</a>
+      </div>
+    </div>
+  </main>
+</template>
+
+
+<style>
+main {
+  height: 90%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.titulo {
+  display: flex;
+  justify-content: center;
+  font-size: 32px;
+  margin-bottom: 50px;
+  margin-top: 50px;
+}
+</style>
+
+
 <script lang="ts">
 import AuthenticationService from '@/services/AuthenticationService'
 import type { IUserData } from '../interfaces/user-data.ts';
@@ -28,47 +74,5 @@ export default {
 
 </script>
 
-<style>
-main {
-  height: 90%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 
-.titulo {
-  display: flex;
-  justify-content: center;
-  font-size: 32px;
-  margin-bottom: 50px;
-  margin-top: 50px;
-}
-</style>
-
-<template>
-  <main>
-    <div class="login">
-      <div class="titulo text-default">
-        Login
-      </div>
-      <div class="mb-3">
-        <label for="formGroupExampleInput" class="text-default">Email</label>
-        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Email" v-model="email">
-      </div>
-      <div class="mb-3">
-        <label for="formGroupExampleInput2" class="text-default">Senha</label>
-        <input type="password" class="form-control" id="formGroupExampleInput2" placeholder="Senha" v-model="password">
-      </div>
-      <div class="mb-3">
-        <button type="button" class="btn btn-outline-warning btn-sm" @click="login">Login</button>
-      </div>
-      <div class="mb-3">
-        <a href="/esqueceu-senha">Esqueceu a senha?</a>
-        <span> | </span>
-        <a href="/criar-conta">Criar conta</a>
-      </div>
-    </div>
-  </main>
-</template>
 
