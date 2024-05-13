eff5610b755d0f4970538ae8ea52433329b16e5e
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2024-05-13 01:11:25 -0300

    .env correct form

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -2,7 +2,8 @@ import { KJUR, KEYUTIL } from 'jsrsasign';
 
 export const CheckToken = async (token: string): Promise<boolean> => {
   try {
-    const secret = '944e72fa-a3a6-414d-be07-49dc89b8da35';
+    const secret = import.meta.env.VITE_TOKEN
+    console.log('SECRET ', secret)
     const decodedToken = KJUR.jws.JWS.verifyJWT(token, secret, {alg: ['HS256']});
     console.log('decodedToken', decodedToken)
     return true;
